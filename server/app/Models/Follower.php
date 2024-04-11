8aa0974d88c64fb7cec5217d5464be114c179e60
Haki Hoti <haki.hoti@student.uni-pr.edu>
2024-04-11 17:24:11 +0200

    Create Follower.php

---
+++ server/app/Models/Follower.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Follow extends Model
+{
+    protected $table = 'follows';
+
+
+    protected $fillable = [
+        'follower_user_id',
+        'followed_user_id',
+
+    ];
+
+    public function follower()
+    {
+
+        return $this->belongsTo(User::class, 'follower_user_id');
+        
+    }
+
+    public function followed()
+    {
+        return $this->belongsTo(User::class, 'followed_user_id');
+    }
+}
